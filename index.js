3f3ee12f7905e0dd468aa2072be36b4cfb2b8d9b
Sumit Dubey <sumitkumardubey1180@gmail.com>
2024-07-22 21:49:55 +0530

    final

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 $(".button1").click(()=>{
-    $(".part2")[0].scrollIntoView({
+    $(".container")[0].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "start" // or "end"
+    });
+})
+$(".button2").click(()=>{
+    $(".container")[1].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "start" // or "end"
+    });
+})
+$(".button3").click(()=>{
+    $(".container")[2].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "start" // or "end"
+    });
+})
+$(".button4").click(()=>{
+    $(".container")[3].scrollIntoView({
         behavior: "smooth", // or "auto" or "instant"
         block: "start" // or "end"
     });
